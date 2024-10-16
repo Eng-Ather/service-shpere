01fdef1bf39169628db40b9dc1f8ce385224bf1e
Ather Ali Siddiqui <engr.ather.93@gmail.com>
2024-10-16 13:39:31 +0500

    edit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,17 +1,47 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
-"use client"
+// import {
+//   getAuth,
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword
+// } from "firebase/auth";
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCmGTBipAOPJbBhfvDjUU52SlWNGrGKPOk",
+//   authDomain: "service-for-sell.firebaseapp.com",
+//   projectId: "service-for-sell",
+//   storageBucket: "service-for-sell.appspot.com",
+//   messagingSenderId: "394236930876",
+//   appId: "1:394236930876:web:f9ad00d25de6da4dea0c39",
+//   measurementId: "G-T0DGWD5EDT"
+// };
+
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const auth = getAuth(app);
+
+// export{
+//   auth,
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword
+
+// }
+
+
+// ____________
+
+"use client";
 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
-} from "firebase/auth";
-
-
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCmGTBipAOPJbBhfvDjUU52SlWNGrGKPOk",
   authDomain: "service-for-sell.firebaseapp.com",
@@ -22,15 +52,22 @@ const firebaseConfig = {
   measurementId: "G-T0DGWD5EDT"
 };
 
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialize analytics only if in the browser
+let analytics;
+if (typeof window !== 'undefined') {
+  const { getAnalytics } = require("firebase/analytics");
+  analytics = getAnalytics(app);
+}
+
+// Initialize authentication
 const auth = getAuth(app);
 
-export{
+// Export the auth methods and auth instance
+export {
   auth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword
-
-}
+};
